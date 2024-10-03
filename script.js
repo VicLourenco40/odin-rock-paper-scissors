58e54e7b62d88a31925890da48d43ae3197099e0
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-03 16:10:16 +0100

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
-    switch(randomNumber) {
+    switch (randomNumber) {
         case 0:
             return 'rock';
+
         case 1:
             return 'paper';
+
         default:
             return 'scissors';
     }
+}
+
+function getHumanChoice() {
+    let input;
+
+    while (input !== 'rock' &&
+           input !== 'paper' &&
+           input !== 'scissors')
+    {
+        input = prompt('Rock, paper or scissors?').toLowerCase();
+    }
+
+    return input;
 }
