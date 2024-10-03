a0ec8fd624109b00d37350ac22581b17a4f2c070
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-03 16:36:09 +0100

    Add playRound and capitalizeWord functions

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getHumanChoice() {
+    let input;
+
+    while (input !== 'rock' &&
+           input !== 'paper' &&
+           input !== 'scissors')
+    {
+        input = prompt('Rock, Paper or Scissors?').toLowerCase();
+    }
+
+    return input;
+}
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
@@ -13,15 +29,28 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let input;
-
-    while (input !== 'rock' &&
-           input !== 'paper' &&
-           input !== 'scissors')
-    {
-        input = prompt('Rock, paper or scissors?').toLowerCase();
-    }
-
-    return input;
+function capitalizeWord(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Tie! Both players picked ${capitalizeWord(humanChoice)}.`);
+    }
+    else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+             (humanChoice === 'paper' && computerChoice === 'rock') ||
+             (humanChoice === 'scissors' && computerChoice === 'paper'))
+    {
+        humanScore++;
+        console.log(`You win! ${capitalizeWord(humanChoice)} beats ${capitalizeWord(computerChoice)}.`);
+    }
+    else {
+        computerScore++;
+        console.log(`You lose! ${capitalizeWord(computerChoice)} beats ${capitalizeWord(humanChoice)}.`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
